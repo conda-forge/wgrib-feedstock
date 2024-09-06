f9588cd0635ccede9c5dc3ddf6870c3592c0fdfc
Isuru Fernando <isuruf@gmail.com>
2024-09-06 16:32:11 -0500

    Update bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,17 @@
-copy "%RECIPE_DIR%\build.sh" .
+echo source %SYS_PREFIX:\=/%/etc/profile.d/conda.sh    > conda_build.sh
+echo conda activate "${PREFIX}"                       >> conda_build.sh
+echo conda activate --stack "${BUILD_PREFIX}"         >> conda_build.sh
+echo CONDA_PREFIX=${CONDA_PREFIX//\\//}               >> conda_build.sh
+echo "make -f makefile CC=$CC"                        >> conda_build.sh
+
 set PREFIX=%PREFIX:\=/%
+set BUILD_PREFIX=%BUILD_PREFIX:\=/%
+set CONDA_PREFIX=%CONDA_PREFIX:\=/%
 set SRC_DIR=%SRC_DIR:\=/%
-set MSYSTEM=MINGW%ARCH%
+set MSYSTEM=UCRT64
 set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
-bash -lc "make -f makefile"
-copy wgrib.exe %LIBRARY_BIN%\
+bash -lc "./conda_build.sh"
 if errorlevel 1 exit 1
+
+copy wgrib.exe %LIBRARY_BIN%\
